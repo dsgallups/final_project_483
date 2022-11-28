fd4acfa358128197983bd7386983165446d3f694
Daniel Gallups <dgallups@purdue.edu>
2022-11-28 13:39:05 -0500

    additional file

--- tf_pong/tfmain.py
+++ tf_pong/tfmain.py
@@ -134,22 +134,7 @@ def run_tf(config):
 
 
 def test_ai(config):
-    width, height = 700, 500
-    window = pygame.display.set_mode((width, height))
-    with open("best.pickle", "rb") as f:
-        winner = pickle.load(f)
-
-    game = PongGame(window, width, height)
-    game.test_ai(winner, config)
-
+    
 
 if __name__ == "__main__":
-    local_dir = os.path.dirname(__file__)
-    config_path = os.path.join(local_dir, "config.txt")
-
-    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
-                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
-                         config_path)
     
-    # run_neat(config)
-    test_ai(config)
